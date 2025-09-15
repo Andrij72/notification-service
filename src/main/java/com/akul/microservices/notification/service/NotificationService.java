ff90e5b1f5cdca340f83cd4d21ca1fb2ebb37ee1
Andrij72 <akulmmm@gmeil.com>
2025-09-15 17:04:31 +0200

    Initialization Notification Service

---
+++ src/main/java/com/akul/microservices/notification/service/NotificationService.java
@@ -0,0 +1,54 @@
+package com.akul.microservices.notification.service;
+
+import com.akul.microservices.order.event.OrderPlacedEvent;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+/**
+ * NotificationService.java
+ *
+ * @author Andrii Kulynch
+ * @version 1.0
+ * @since 9/14/2025
+ */
+@Service
+@AllArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final JavaMailSender mailSender;
+
+    @KafkaListener(topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent) {
+
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,true);
+            messageHelper.setFrom("from@example.com ");
+            messageHelper.setTo(orderPlacedEvent.getEmail());
+            messageHelper.setSubject(String.format("Your Order with OrderNbr %s placed successfully", orderPlacedEvent.getOrderNbr()));
+            messageHelper.setText(String.format("""
+                            Hi
+                            
+                            Your Order with OrderNbr %s placed successfully.
+                            
+                            Best Regards
+                            Cloud Shop
+                            """,
+                    orderPlacedEvent.getOrderNbr()));
+            log.info("Order Notification email sent !!!");
+        };
+        try {
+            mailSender.send(messagePreparator);
+        }catch (MailException e) {
+            log.error("Exception sending email",e);
+            throw new RuntimeException("Exception while sending email to springshop@email.com",e);
+        }
+
+    }
+}
